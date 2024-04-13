961cc737de914a76b9da31a7fe6176707d69c4dd
Syedabanog-1 <160377198+Syedabanog-1@users.noreply.github.com>
2024-04-13 12:58:43 +0500

    Add files via upload

---
+++ main9.js
@@ -0,0 +1,6 @@
+/* 9.	Favorite Number: Store your favorite number in a variable.
+ Then, using that variable, create a message that reveals your
+  favorite number.Print that message*/
+var favourite_Number = 9;
+var message = 'My favourite number is:' + favourite_Number;
+console.log(message);
