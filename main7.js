cde80cdf0b91dc89e8486310a35675d563d1e97e
Syedabanog-1 <160377198+Syedabanog-1@users.noreply.github.com>
2024-04-13 12:57:34 +0500

    Add files via upload

---
+++ main7.js
@@ -0,0 +1,15 @@
+/*Number Eight: Write addition, subtraction, multiplication,
+ and division operations that each result in the number 8.
+  Be sure to enclose your operations in print statements to see the results*/
+// Addition
+var num1 = 5 + 0;
+console.log("Result is:", num1);
+//Subtraction
+var num2 = 10 - 5;
+console.log("Result is:", num2);
+// Multiplication
+var num3 = 5 * 1;
+console.log("Result is:", num3);
+// Division
+var num4 = 25 / 5;
+console.log("Result is:", num3);
